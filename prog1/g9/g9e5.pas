f4729d1e574f93449a36eb70b65836127ace1342
gaillour <juangaillour.8@gmail.com>
2024-05-21 16:49:59 -0300

    g9e5

--- prog1/g9/g9e5.pas
+++ prog1/g9/g9e5.pas
@@ -3,7 +3,7 @@ program g9e5;
 type
     TR1=record
         matricula:integer;
-        mat,fis,qui:byte;
+        mat,fis,qui:byte;//(real)
     end;
     TA1=file of TR1;
     TV=array[1..50] of integer;
@@ -34,10 +34,36 @@ begin
     close(A1);
 end;
 
-procedure listado();
+function busca(V:TV;matricula:integer):byte;
 var
+    i:byte;
 begin
-   
+    i:=0;
+    while V[i]<>matricula do
+        i:=i+1;
+    busca:=i;
+end;
+
+{para  FISICA  II,  tener  aprobados  MATEMATICA  y  FISICA  I,  
+ para QUIMICA II, tener aprobado QUIMICA I. (se aprueba con NOTA >=4)}
+procedure listado(var A1:TA1; var A2:TA2;var V:TV);
+var
+    R1:TR1;
+    R2:TR2;
+
+begin
+    reset(A1);reset(A2);
+    read(A2,R2);
+    writeln('   Rechazados');
+    while R2.matricula<>111 do
+    begin
+        seek(A1,busca(V,R2.matricula)-1);
+        read(A1,R1);
+        if ((R2.f2) and (R1.mat<4) and (R1.fis<4)) or ((R2.q2) and (R1.qui<4)) then
+            writeln('      ',R2.matricula);
+        read(A2,R2);
+    end;
+    Close(A1);close(A2);
 end;
 
 
@@ -45,11 +71,12 @@ var
     A1:TA1;
     A2:TA2;
     V:TV;
-    i,n:byte;
+    n:byte;
 
 begin
     assign(A1,'ALUMNOS.DAT');
     assign(A2,'INSCRIPTOS.DAT');
     listarAlumnos(A1,V,n);
+    listado(A1,A2,V);
 
 end.
